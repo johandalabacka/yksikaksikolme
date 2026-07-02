d54e368df895b258f3e05853136b986a87234f98
Johan Dahl <johan.dalabacka@gmail.com>
2026-07-03 01:04:03 +0200

    Disable active button

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <header class="bg-light py-3 text-center">
       <h1>Learn Finnish numbers, days and months 🇫🇮</h1>
       <p v-if="state === 'start'" class="lead">Press the button to start</p>
-      <button type="button" class="btn btn-primary btn-sm" @click="startListen">Listen</button>
-      <button type="button" :disable="!isSpeechRecognitionSupported()" class="btn btn-primary btn-sm ms-3" @click="startSpeak">Speak</button>
-      <button type="button" class="btn btn-primary btn-sm ms-3" @click="startDayMonth">Day & Month</button>
+      <button type="button" :disabled="state === 'listen'" class="btn btn-primary btn-sm" @click="startListen">Listen</button>
+      <button type="button" :disabled="state === 'speak' || !isSpeechRecognitionSupported()" class="btn btn-primary btn-sm ms-3" @click="startSpeak">Speak</button>
+      <button type="button" :disabled="state === 'daymonth'" class="btn btn-primary btn-sm ms-3" @click="startDayMonth">Day & Month</button>
       <div v-if="state !== 'start'" class="mt-3">
         <span class="text-muted me-3">🙂 Correct: {{ numberStore.countCorrect }}</span>
         <span class="me-3">😟 Wrong: {{ numberStore.countWrong }}</span>
